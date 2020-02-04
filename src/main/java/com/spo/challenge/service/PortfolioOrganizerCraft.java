4abdcd5b3a3dd174afb694a77e4a16d31a178985
rafaelcornelio <bautistacornelio.rafael@gmail.com>
2020-02-04 11:30:28 +0100

    adding Organizer Documentation and fix the name of the build directory

--- src/main/java/com/spo/challenge/service/PortfolioOrganizerCraft.java
+++ src/main/java/com/spo/challenge/service/PortfolioOrganizerCraft.java
@@ -8,9 +8,18 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+/**
+ *
+ */
 @Service
 public class PortfolioOrganizerCraft implements PortfolioOrganizer {
 
+    /**
+     * method to organize the workforce for cleaning all the buildings
+     *
+     * @param portfolioInfoDto
+     * @return Mono<CleaningOrganizersDto>
+     */
     @Override
     public Mono<CleaningOrganizersDto> organize(final PortfolioInfoDto portfolioInfoDto) {
 
@@ -18,13 +27,19 @@ public class PortfolioOrganizerCraft implements PortfolioOrganizer {
         final Integer jrCapacity = portfolioInfoDto.getJrCapacity();
         final Integer srCapacity = portfolioInfoDto.getSrCapacity();
 
-        final List<CleaningOrganizeDto> results = structures.stream()
-                                                            .map(rooms -> organizeBuilding(jrCapacity, srCapacity, rooms))
-                                                            .collect(Collectors.toList());
+        final List<CleaningOrganizeDto> results = structures.stream().map(rooms -> organizeBuilding(jrCapacity, srCapacity, rooms)).collect(Collectors.toList());
 
         return Mono.just(CleaningOrganizersDto.builder().list(results).build());
     }
 
+    /**
+     * method to organize the workforce for cleaning one buildings
+     *
+     * @param jrCapacity
+     * @param srCapacity
+     * @param rooms
+     * @return CleaningOrganizeDto
+     */
     private CleaningOrganizeDto organizeBuilding(final Integer jrCapacity, final Integer srCapacity, final Integer rooms) {
 
         final CleaningOrganizeDto solutionWithSr = calculateSolutionWithSr(jrCapacity, srCapacity, rooms);
@@ -33,6 +48,14 @@ public class PortfolioOrganizerCraft implements PortfolioOrganizer {
         return chooseBestSolution(jrCapacity, srCapacity, rooms, solutionWithSr, solutionWithJr);
     }
 
+    /**
+     * method to calculate the best workforce for cleaning one buildings using Jr as a mayor workforce
+     *
+     * @param jrCapacity
+     * @param srCapacity
+     * @param rooms
+     * @return
+     */
     private CleaningOrganizeDto calculateSolutionWithJr(final Integer jrCapacity, final Integer srCapacity, final Integer rooms) {
 
         final int jrNeededToCleanBuilding = rooms / jrCapacity;
@@ -48,6 +71,15 @@ public class PortfolioOrganizerCraft implements PortfolioOrganizer {
         return CleaningOrganizeDto.builder().rooms(rooms).jrNumber(numberJrNeededToFinishTheJob).srNumber(1).build();
     }
 
+    /**
+     *
+     * method to calculate the best workforce for cleaning one buildings using Sr as a mayor workforce
+     *
+     * @param jrCapacity
+     * @param srCapacity
+     * @param rooms
+     * @return
+     */
     private CleaningOrganizeDto calculateSolutionWithSr(final Integer jrCapacity, final Integer srCapacity, final Integer rooms) {
 
         final int srNeededToCleanBuilding = rooms / srCapacity;
@@ -60,6 +92,18 @@ public class PortfolioOrganizerCraft implements PortfolioOrganizer {
         return CleaningOrganizeDto.builder().rooms(rooms).jrNumber(0).srNumber(srNeededToCleanBuilding + 1).build();
     }
 
+    /**
+     *
+     * method to calculate the best workforce between a solution with Jr as mayor workforce
+     * and Sr as mayor workforce
+     *
+     * @param jrCapacity
+     * @param srCapacity
+     * @param rooms
+     * @param solutionWithSr
+     * @param solutionWithJr
+     * @return
+     */
     private CleaningOrganizeDto chooseBestSolution(final Integer jrCapacity,
                                                    final Integer srCapacity,
                                                    final Integer rooms,
@@ -76,6 +120,15 @@ public class PortfolioOrganizerCraft implements PortfolioOrganizer {
         return workForceFromSrSolution > workForceFromJrSolution ? solutionWithJr : solutionWithSr;
     }
 
+    /**
+     *
+     * method to calculate workforce for a given cleaning solutions
+     *
+     * @param jrCapacity
+     * @param srCapacity
+     * @param solution
+     * @return
+     */
     private int calculateWorkForce(final Integer jrCapacity, final Integer srCapacity, final CleaningOrganizeDto solution) {
         final int jrNumber = solution.getJrNumber();
         final int srNumber = solution.getSrNumber();
